fde2c2381f0682eaafd6fd15e3f0e87f4646001d
Termication <terminalkarabo@gmail.com>
2023-12-30 13:06:30 +0200

    --

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include <c:/raylib/mingw64/include/raylib.h>
+#include "c:/raylib/raylib/src/raylib.h"
+
+int main() {
+    // Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Raylib Example");
+
+    SetTargetFPS(60);
+
+    // Main game loop
+    while (!WindowShouldClose()) {
+        // Update
+
+        // Draw
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        // Draw a colored rectangle
+        DrawRectangle(screenWidth / 4, screenHeight / 4, screenWidth / 2, screenHeight / 2, MAROON);
+
+        DrawText("Raylib Example", 10, 10, 20, DARKGRAY);
+
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow();
+
+    return 0;
+}
