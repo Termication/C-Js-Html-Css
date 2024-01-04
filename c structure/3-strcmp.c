b32adc52f9e68f70c554faf72691abf220c31e6e
Terminal <terminalkarabo@gmail.com>
2024-01-04 14:43:20 +0200

    string compare

---
+++ c structure/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _strcmp - The main function
+  *
+  * @s1: Function parameter
+  *
+  * @s2: Function parameter
+  *
+  * Return: s1, s2
+  */
+int _strcmp(char *s1, char *s2)
+{
+int k = 0;
+
+while (s1[k] != '\0' && s2[k] != '\0')
+{
+	if (s1[k] != s2[k])
+	{
+	return (s1[k] - s2[k]);
+	}
+	k++;
+	}
+	return (0);
+}
