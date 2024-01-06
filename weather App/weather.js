3d10a506b213bf3a6644f6504e4143bc3038c0ab
Terminal <terminalkarabo@gmail.com>
2024-01-06 15:45:10 +0200

    weather app ip added

--- weather App/weather.js
+++ weather App/weather.js
@@ -1,8 +1,11 @@
 const key = '9f4c1fcdecc54ff62573c3763650ee8c';
 
-function search(){
-    const phrase = document.querySelector(selectors: 'input[type="text"]').value;
-    fetch(input: '');
+async function search(){
+    const phrase = document.querySelector('input[type="text"]').value;
+    //console.log(phrase);
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${phrase}&limit=5&appid=${key}`);
+    const data = await Response.json();
+    console.log(data);
 }
 const debouncedSearch = _.debounce(() => {
     search();
