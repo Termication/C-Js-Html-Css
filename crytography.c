3b2879acb6a7454c2611fde893cdb2f5b01b72ef
Terminal <terminalkarabo@gmail.com>
2024-01-11 16:43:18 +0200

    encrypted message program

---
+++ crytography.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void encrypt(char message[]){
+    
+    for (int i = 0; message[i] != '\0'; i++){
+
+        if(message[i] >= 'a' && message[i] <= 'z'){
+            message[i] = (message[i] - 1 - 'a') % 26 + 'a' ;
+
+        }
+
+    }
+}
+int main(){
+    char message[100] = {"this was fun and we should do that again"};
+
+    encrypt(message);
+
+    printf("encrypted message: %s", message);
+}
