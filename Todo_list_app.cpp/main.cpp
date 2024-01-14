a3cb83e9c08027a1713f42d43fd51e08a2c374cf
Terminal <terminalkarabo@gmail.com>
2024-01-14 15:20:03 +0200

    iterator added

--- Todo_list_app.cpp/main.cpp
+++ Todo_list_app.cpp/main.cpp
@@ -20,13 +20,20 @@ int main(){
     
     std::string version = "v0.0.2";
     std::list<TodoItem> todoItems;
+    std::list<TodoItem>::iterator it;
 
     todoItems.clear();
 
     while(1){
         system("cls");
         std::cout << "Todo List Maker - " << version << std::endl;
-        std::endl << std::endl;
+        std::cout << std::endl << std::endl;
+
+        for (it = todoItems.begin(); it != todoItems.end(); it++){
+            std::cout << it->getId() << " | " << it->getDescription() << " | " 
+              << it->isCompleted() << std::endl;
+        }
+
     }
 
 }
