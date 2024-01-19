8de03b183eeb3e202c9f7c7762a45fb2a5b8b339
Terminal <terminalkarabo@gmail.com>
2024-01-19 16:52:00 +0200

    password connected to html

---
+++ Todo_list_app.cpp/Password_generator/generator.js
@@ -0,0 +1,31 @@
+
+function generate(){
+    let dictionary = '';
+    if (document.getElementById(lowercaseCb).checked){
+        dictionary = +'qwertyuiopasdfghjklzxcvbnm';
+    }
+    if (document.getElementById(uppercaseCb).checked){
+        dictionary = +'QWERTYUIOPASDFGHJKLZXCVBNM';
+    }
+    if (document.getElementById(digitsCb).checked){
+        dictionary = +'0123456789';
+    }
+    if (document.getElementById(specialsCb).checked){
+        dictionary = +' !@#$%^&*()_-+={};<>:';
+    }
+
+    const length = document.querySelector(input[type="range"]).value;
+
+    if (length < 1 || dictionary.length === 0){
+        return;
+    }
+    let password = '';
+    for (int i = 0; i < length; i++){
+        const pos = Math.floor(Math.random() * dictionary.length);
+        password += dictionary[pos];
+    }
+
+    document.querySelector(input[type="text"]).value = password;
+}
+
+generate();
