10fa3355ef856027610ca4a4692d1be271862163
Terminal <terminalkarabo@gmail.com>
2024-01-19 17:02:27 +0200

    code debugging

--- Todo_list_app.cpp/Password_generator/generator.js
+++ Todo_list_app.cpp/Password_generator/generator.js
@@ -2,30 +2,30 @@
 function generate(){
     let dictionary = '';
     if (document.getElementById(lowercaseCb).checked){
-        dictionary = +'qwertyuiopasdfghjklzxcvbnm';
+        dictionary += 'qwertyuiopasdfghjklzxcvbnm';
     }
     if (document.getElementById(uppercaseCb).checked){
-        dictionary = +'QWERTYUIOPASDFGHJKLZXCVBNM';
+        dictionary += 'QWERTYUIOPASDFGHJKLZXCVBNM';
     }
     if (document.getElementById(digitsCb).checked){
-        dictionary = +'0123456789';
+        dictionary += '0123456789';
     }
     if (document.getElementById(specialsCb).checked){
-        dictionary = +' !@#$%^&*()_-+={};<>:';
+        dictionary += ' !@#$%^&*()_-+={};<>:';
     }
 
-    const length = document.querySelector(input[type="range"]).value;
+    const length = document.querySelector('input[type="range"]').value;
 
     if (length < 1 || dictionary.length === 0){
         return;
     }
     let password = '';
-    for (int i = 0; i < length; i++){
+    for (let i = 0; i < length; i++){
         const pos = Math.floor(Math.random() * dictionary.length);
         password += dictionary[pos];
     }
 
-    document.querySelector(input[type="text"]).value = password;
+    document.querySelector('input[type="text"]').value = password;
 }
 
 generate();
