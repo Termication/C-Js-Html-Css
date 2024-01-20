2826ef236ba2a61141d0dac7dca4a9902c04f22c
Terminal <terminalkarabo@gmail.com>
2024-01-20 16:41:22 +0200

    array of objects

--- arrayobjects.cpp
+++ arrayobjects.cpp
@@ -1,42 +1,31 @@
 #include <iostream>
-
 using namespace std;
 
-class Student{
-  public:
+class Student {
+public:
     string name;
     int age;
     char gender;
     float grade;
 
-    Student(string Name, int Age, char Gender, float Grade){
-        Name = name;
-        Age = age;
-        Gender = gender;
-        Grade = grade;
-    }
+    // Constructor with initialization list
+    Student(string Name, int Age, char Gender, float Grade) 
+        : name(Name), age(Age), gender(Gender), grade(Grade) {}
+
+    // Default constructor
+    Student() : name(""), age(0), gender(' '), grade(0.0) {}
 };
 
-int main(){
+int main() {
+    // Student s1; // You can create an object using the default constructor if needed
 
-    /*
-    -what is arrays of objects
-    -how are they different from arraysof primitive data types
-    -why and when should you use objects in the arrays
-    -how do you access individual objects int the array
-    -how do you access and modify object properties in the array
-    -how to use loops to perform operations on arrays of objects
-    -most common errors that can happen 
-    */
-   int luckynumbers[5] = {1,4,5,2,10};
+    // Initializing an array of objects
+    Student students[3] = {{"Innocent", 27, 'm', 12}, {"Elon", 25, 'm', 10}, {"Kane", 10, 'm', 9}};
 
-   //Student s1;
+    // Accessing and printing information about each student
+    for (int i = 0; i < 3; i++) {
+        cout << "STUDENT " << i << " - Name: " << students[i].name << ", Age: " << students[i].age << endl;
+    }
 
-   /*s1.name = "Innocent";
-   s1.age = 25;
-   s1.gender = 'm';
-   s1.grade = 12;*/
-
-   Student students[3] = {{"innocent", 27, 'm', 12},{"Elon", 25, 'm', 10}, {"Kane", 10, 'm', 9}};
-
+    return 0;
 }
