89ad72918d506b9ce466f6c14b9076e5e1392164
Terminal <terminalkarabo@gmail.com>
2024-01-20 16:02:48 +0200

    array object practice

---
+++ arrayobjects.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Student{
+    string name;
+    int age;
+    char gender;
+    float grade;
+};
+
+int main(){
+
+    /*
+    -what is arrays of objects
+    -how are they different from arraysof primitive data types
+    -why and when should you use objects in the arrays
+    -how do you access individual objects int the array
+    -how do you access and modify object properties in the array
+    -how to use loops to perform operations on arrays of objects
+    -most common errors that can happen 
+    */
+   int luckynumbers[5] = {1,4,5,2,10};
+    
+}
