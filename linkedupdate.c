d51d656814afca55cc53ab217841031f0ccb1ccd
Terminal <terminalkarabo@gmail.com>
2024-01-27 15:00:04 +0200

    linkedlist working, can take input and update into list

--- linkedupdate.c
+++ linkedupdate.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
-#include <stdio.h>
-
+#include <stdlib.h>
 
 struct Node {
     int data;
     struct Node* next;
 };
+
 struct Node* head; // global var, pointer to node, can be accessed anywhere
 
-void Insert(int x){
+void Insert(int x) {
     struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-    temp->data = x; 
-    temp->next = head; // update the next pointer to point to current
-    head = temp; // update the head to newly created node
-
+    temp->data = x;
+    temp->next = head; // Update the next pointer to point to the current head
+    head = temp;       // Update the head to the newly created node
 }
 
-void Print(){
+void Print() {
     struct Node* temp = head;
     printf("List is: ");
-    while(temp != NULL){
+    while (temp != NULL) {
         printf(" %d", temp->data);
-        temp = temp -> next;
+        temp = temp->next;
     }
     printf("\n");
 }
 
-int main (){
+int main() {
     head = NULL; // empty list;
 
     printf("How many numbers?\n");
@@ -35,10 +34,12 @@ int main (){
 
     scanf("%d", &n);
 
-    for (i = 0; i < n; i++){
+    for (i = 0; i < n; i++) {
         printf("Enter a number: \n");
         scanf("%d", &x);
         Insert(x);
         Print();
     }
-} 
+
+    return 0;
+}
