9acf2ced526b7ef72dd33e914dfd3123b229ba39
Terminal <terminalkarabo@gmail.com>
2024-01-29 17:56:18 +0200

    first commit of the day

--- MAT.cpp
+++ MAT.cpp
@@ -6,7 +6,7 @@ int main(){
    // conversion for temperature
 
 int Fahrenheit;
-std::cout << "Enter Your Fahrenheit You Want To Convert To Celsious: ";
+std::cout << "Enter  Your Fahrenheit  You  Want To Convert To Celsious: ";
 std::cin >> Fahrenheit;
 std::cout << "Celsious : " << 5 * (Fahrenheit - 32) / 9;
 
