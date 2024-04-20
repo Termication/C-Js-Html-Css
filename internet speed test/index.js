4cfaba831ff9359bac3a7eaee6878bd3eb4e8c2e
Terminal <terminalkarabo@gmail.com>
2024-04-20 17:12:43 +0200

    js update speed calculation

--- internet speed test/index.js
+++ internet speed test/index.js
@@ -16,5 +16,33 @@ let imageApi = "https://source.unsplash.com/random?topic=nature";
 
 //when image loads
 image.onload = async function(){
+    endTime = new Date().getTime();
 
+    //get image size
+    await fetch(imageApi).then((response) => {
+        imageSize = response.headers.get("content-length");
+        calculateSpeed();
+    });
+
+};
+
+//calculate function
+
+function calculateSpeed(){
+    //time taken in sec
+    let timeDuration = (endTime - startTime) / 1000;
+    
+    let loadedBits = imageSize * 8;
+    let speedInBts = loadedBits / timeDuration;
+    let speedInKbs = speedInBts / 1024;
+    let speedInMbs = speedInKbs / 1024;
+
+
+    totalBitSpeed += speedInBts;
+    totalKbSpeed += speedInKbs;
+    totalMbSpeed += speedInMbs;
+
+    testCompleted++;
+
+    
 }
